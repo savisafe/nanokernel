1072b0e810627b4ec5c758eec99dca6ed0770f4c
savisafe <savisafe@vk.com>
2026-05-05 01:19:55 +0500

    remove: config-management

--- src/modules/config-management/config-management.types.ts
+++
@@ -1,8 +0,0 @@
-import type { ResolvedBotConfiguration } from "../bot-configuration/bot-configuration.types";
-import type { ResolvedLlmPromptProfile } from "../prompt-profile/prompt-profile.types";
-
-/** Ресурсы бота для админского теста / предпросмотра (ещё не собранные в DialogRuntimeSnapshot). */
-export interface ResolvedDialogResourceBundle {
-  bot: ResolvedBotConfiguration;
-  profile: ResolvedLlmPromptProfile;
-}
